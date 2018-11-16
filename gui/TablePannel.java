4642e8dc8cc270a048865d0ba4ec1e8bc95725ac
Жилич Александр Иванович <alexander.zhilich@btc.info>
2018-11-16 17:45:54 +0300

    select table row

--- gui/TablePannel.java
+++ gui/TablePannel.java
@@ -4,6 +4,8 @@ import model.Person;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -19,15 +21,25 @@ public class TablePannel extends JTable {
         personTableModel = new PersonTableModel();
         table = new JTable(personTableModel);
         popupMenu = new JPopupMenu();
-        JMenuItem remuve = new JMenuItem("Delete row");
-        popupMenu.add(remuve);
+        JMenuItem removeItem = new JMenuItem("Delete row");
+        popupMenu.add(removeItem);
 
         table.addMouseListener(new MouseAdapter(){
             @Override
             public void mousePressed(MouseEvent e) {
+                int row = table.rowAtPoint(e.getPoint());
+                table.getSelectionModel().setSelectionInterval(row,row);
                 if (e.getButton() == MouseEvent.BUTTON3){
                     popupMenu.show(table,e.getX(),e.getY());
                 }
+            }
+        });
+
+        removeItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = table.getSelectedRow();
+                
             }
         });
 
